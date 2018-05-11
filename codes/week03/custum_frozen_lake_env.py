5aeb7196be0a72a551b27e072a1f9c8119ecb823
Kyungjae Lee <dlxhrl@snu.ac.kr>
2018-05-12 08:37:49 +0900

    Add files via upload

---
+++ codes/week03/custum_frozen_lake_env.py
@@ -0,0 +1,109 @@
+import numpy as np
+import sys
+from six import StringIO, b
+
+from gym import utils
+from gym.envs.toy_text import discrete
+
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+MAPS = {
+    "4x4": [
+        "SFFF",
+        "FHFH",
+        "FFFH",
+        "HFFG"
+    ],
+    "8x8": [
+        "SFFFFFFF",
+        "FFFFFFFF",
+        "FFFHFFFF",
+        "FFFFFHFF",
+        "FFFHFFFF",
+        "FHHFFFHF",
+        "FHFFHFHF",
+        "FFFHFFFG"
+    ],
+}
+
+class CustumFrozenLakeEnv(discrete.DiscreteEnv):
+    metadata = {'render.modes': ['human', 'ansi']}
+
+    def __init__(self, desc=None, map_name="4x4",is_slippery=True):
+        if desc is None and map_name is None:
+            raise ValueError('Must provide either desc or map_name')
+        elif desc is None:
+            desc = MAPS[map_name]
+        self.desc = desc = np.asarray(desc,dtype='c')
+        self.nrow, self.ncol = nrow, ncol = desc.shape
+        self.reward_range = (0, 1)
+
+        nA = 4
+        nS = nrow * ncol
+
+        isd = np.array(desc == b'S').astype('float64').ravel()
+        isd /= isd.sum()
+
+        P = {s : {a : [] for a in range(nA)} for s in range(nS)}
+
+        def to_s(row, col):
+            return row*ncol + col
+        
+        def inc(row, col, a):
+            if a==0: # left
+                col = max(col-1,0)
+            elif a==1: # down
+                row = min(row+1,nrow-1)
+            elif a==2: # right
+                col = min(col+1,ncol-1)
+            elif a==3: # up
+                row = max(row-1,0)
+            return (row, col)
+
+        for row in range(nrow):
+            for col in range(ncol):
+                s = to_s(row, col)
+                for a in range(4):
+                    li = P[s][a]
+                    letter = desc[row, col]
+                    if letter in b'GH':
+                        li.append((1.0, s, 0, True))
+                    else:
+                        if is_slippery:
+                            for b in [(a-1)%4, a, (a+1)%4]:
+                                newrow, newcol = inc(row, col, b)
+                                newstate = to_s(newrow, newcol)
+                                newletter = desc[newrow, newcol]
+                                done = bytes(newletter) in b'GH'
+                                if newletter == b'G': rew = 1.0
+                                elif newletter == b'H': rew = -1.0
+                                else: rew = -0.03
+                                li.append((1.0/3.0, newstate, rew, done))
+                        else:
+                            newrow, newcol = inc(row, col, a)
+                            newstate = to_s(newrow, newcol)
+                            newletter = desc[newrow, newcol]
+                            done = bytes(newletter) in b'GH'
+                            rew = float(newletter == b'G')
+                            li.append((1.0, newstate, rew, done))
+
+        super(CustumFrozenLakeEnv, self).__init__(nS, nA, P, isd)
+
+    def render(self, mode='human'):
+        outfile = StringIO() if mode == 'ansi' else sys.stdout
+
+        row, col = self.s // self.ncol, self.s % self.ncol
+        desc = self.desc.tolist()
+        desc = [[c.decode('utf-8') for c in line] for line in desc]
+        desc[row][col] = utils.colorize(desc[row][col], "red", highlight=True)
+        if self.lastaction is not None:
+            outfile.write("  ({})\n".format(["Left","Down","Right","Up"][self.lastaction]))
+        else:
+            outfile.write("\n")
+        outfile.write("\n".join(''.join(line) for line in desc)+"\n")
+
+        if mode != 'human':
+            return outfile
